d2fff0c60919e925f883ec90d546af6f51b77b78
ArgaPahreja <77570727+ArgaPahreja@users.noreply.github.com>
2023-07-03 14:12:53 +0700

    Update requestMethods.js

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "http://edumar-api.vercel.app/";
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
