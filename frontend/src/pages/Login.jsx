5a346d247fff18702a7be47c5958f60443c86d15
ArgaPahreja <77570727+ArgaPahreja@users.noreply.github.com>
2023-07-03 14:47:08 +0700

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
     try {
       // Melakukan permintaan POST ke endpoint login
       axios
-        .post("http://localhost:5000/users/login", userCheck)
+        .post("http://edumar-api.vercel.app/users/login", userCheck)
         .then((response) => {
           console.log(response.status);
           console.log(response.data);
